e9a0df00f132cc51c06f0b5ec2905ad6ea123a69
aleaplicativos <122333431+aleaplicativos@users.noreply.github.com>
2023-06-20 11:09:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,30 +9,26 @@ function searchWords() {
     .catch(error => console.log('Ocorreu um erro:', error));
 }
 
+// Função para exibir os resultados
+function displayResults(words) {
+  const resultsContainer = document.getElementById('results-container');
+  resultsContainer.innerHTML = '';
 
+  if (words.length === 0) {
+    resultsContainer.innerHTML = 'Nenhuma palavra próxima encontrada.';
+  } else {
+    const list = document.createElement('ul');
+    words.forEach(word => {
+      const listItem = document.createElement('li');
+      listItem.textContent = word;
+      list.appendChild(listItem);
+    });
+    resultsContainer.appendChild(list);
   }
-  
-  // Função para exibir os resultados
-  function displayResults(words) {
-    const resultsContainer = document.getElementById('results-container');
-    resultsContainer.innerHTML = '';
-  
-    if (words.length === 0) {
-      resultsContainer.innerHTML = 'Nenhuma palavra próxima encontrada.';
-    } else {
-      const list = document.createElement('ul');
-      words.forEach(word => {
-        const listItem = document.createElement('li');
-        listItem.textContent = word;
-        list.appendChild(listItem);
-      });
-      resultsContainer.appendChild(list);
-    }
-  }
-  
-  // Adiciona um listener para o botão de busca
-  document.getElementById('search-button').addEventListener('click', function(event) {
-    event.preventDefault();
-    searchWords();
-  });
-  
+}
+
+// Adiciona um listener para o botão de busca
+document.getElementById('search-button').addEventListener('click', function(event) {
+  event.preventDefault();
+  searchWords();
+});
